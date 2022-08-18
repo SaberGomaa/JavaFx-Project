d7331e5259e9ed731c3e9a13780a82d559ebfa01
sabegomaa261 <sabergomaa261@gmail.com>
2022-08-18 11:11:38 +0200

    Final Commit

---
+++ javaproject/coursesTableviewControllerupdate.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaproject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import models.course;
+import models.register;
+
+/**
+ * FXML Controller class
+ *
+ * @author Ali Tahoon
+ */
+public class coursesTableviewControllerupdate implements Initializable {
+
+    @FXML
+    TextField txtID;
+    @FXML
+    TextField txtName;
+    @FXML
+    private TableView<models.course> tblviewcourses;
+    @FXML
+    private TableColumn<models.course, Integer> id;
+    @FXML
+    private TableColumn<models.course, String> name;
+    String query = null;
+    Connection connection = null;
+    ResultSet resultSet = null;
+    PreparedStatement preparedStatement;
+    models.course registeredCourses = null;
+    ObservableList<models.course> courseList = FXCollections.observableArrayList();
+
+    @FXML
+    private void getClose() {
+
+    }
+
+    @FXML
+    private void Exit(javafx.scene.input.MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+
+    }
+
+    @FXML
+    public void onClear() {
+        txtName.setText(null);
+    }
+
+    @FXML
+    private void getUpdate(javafx.scene.input.MouseEvent event) {
+        try {
+            query = "update  courses set coursesname = " + "'" + txtName.getText() + "'"
+                    + "where courseID = " + txtID.getText();
+            System.out.println(query);
+            connection = database.dbconnection.getConnect();
+            preparedStatement = connection.prepareCall(query);
+            preparedStatement.execute();
+            getRefresh();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @FXML
+    public void getRefresh() {
+        try {
+            courseList.clear();
+            query = "select *from registerd";
+            preparedStatement = connection.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                courseList.add(new course(Integer.parseInt(resultSet.getString("courseID")),
+                resultSet.getString("coursename")));
+                tblviewcourses.setItems(courseList);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(coursesTableviewControllerupdate.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    public void ready() {
+        registeredCourses = tblviewcourses.getSelectionModel().getSelectedItem();
+        txtID.setText(String.valueOf(registeredCourses.getId()));
+ 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        loadData();
+        getRefresh();
+    }
+
+    private void loadData() {
+        connection = database.dbconnection.getConnect();
+        id.setCellValueFactory(new PropertyValueFactory<>("courseID"));
+        name.setCellValueFactory(new PropertyValueFactory<>("coursename"));
+    }
+
+}
